35d6f15da3a22f3959456eab662c909f09994999
Dmitrii Eselidze <dailyadre@gmail.com>
2024-06-06 23:53:37 +0300

    Add files via upload

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dmitrii Eselidze on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
